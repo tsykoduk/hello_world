5d21c1084161d3482085b6b2c5cb74e0b780ba81
Greg Nokes <tsykoduk@gmail.com>
2013-06-26 18:07:20 -0700

    first commit

---
+++ config.ru
@@ -0,0 +1,5 @@
+require "rubygems"
+
+require './web.rb'
+
+run Sinatra::Application
---
+++ web.rb
@@ -0,0 +1,31 @@
+# web.rb
+require 'sinatra'
+helpers do
+          def protected!
+            unless ENV['RACK_ENV'] != 'staging' || authorized?
+                    response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+                    throw(:halt, [401, "Not authorized\n"])
+            end
+          end
+        
+          def authorized?
+            @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+            @auth.provided? && @auth.basic? && @auth.credentials && 
+            @auth.credentials == ['alladin', 'opensesame']
+          end
+      end
+
+get '/' do
+    "Hello, " << ENV['NAME']
+end 
+
+get '/protected' do
+  protected!
+  "Hi there, Mr Secure User Named " << ENV['NAME']
+
+get '/slow' do
+        protected!
+		sleep(2)
+        "Hello, " << ENV['NAME']
+end
+
